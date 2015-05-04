3bf423f761f6f52cda51b0ac8cacb8bc681c5561
Elliot Rosen <elliotr@ece003.ece.cmu.edu>
2015-05-04 11:55:38 -0400

    bunch of things. all my encounter stuff in here too. I think

--- BIST.v
+++ BIST.v
@@ -12,13 +12,14 @@ module rc_count (input clk, clear_l, cin, output value, cout);
 
 endmodule
 
-// 9 bit counter
-module counter512 (
-output [8:0] value,
+// good for counting patterns I think!
+// 10 bit counter
+module counter1024 (
+output [9:0] value,
 output cout, 
 input clk, clear_l, inc);
 
-  wire [7:0] co;
+  wire [8:0] co;
 
   rc_count c0 (clk, clear_l, inc, value[0], co[0]);
   rc_count c1 (clk, clear_l, co[0], value[1], co[1]);
@@ -28,7 +29,8 @@ input clk, clear_l, inc);
   rc_count c5 (clk, clear_l, co[4], value[5], co[5]);
   rc_count c6 (clk, clear_l, co[5], value[6], co[6]);
   rc_count c7 (clk, clear_l, co[6], value[7], co[7]);
-  rc_count c8 (clk, clear_l, co[7], value[8], cout);
+  rc_count c7 (clk, clear_l, co[7], value[8], co[8]);
+  rc_count c8 (clk, clear_l, co[8], value[9], cout);
 
 endmodule
 /*
@@ -42,7 +44,20 @@ module fsm_bist_behavioral (
 endmodule
 */
 
+// init (whenever BIST disabled)
 
+
+
+module fsm_bist (
+output shift, load, run, init,
+input clk, bist_en, 
+
+
+
+
+
+
+// do not use
 module fsm_bist_1h (
 output shift,
 output load,
@@ -379,4 +394,292 @@ module dff1 (output q, input clock, data, set1);
   or (d, data, set1);
   dff f (q, clock, d);
 
+endmodule
+
+
+
+module xor250(
+  input [249:0] x,
+  output y
+);
+
+xor(
+  y,
+
+  x[  0],
+  x[  1],
+  x[  2],
+  x[  3],
+  x[  4],
+  x[  5],
+  x[  6],
+  x[  7],
+  x[  8],
+  x[  9],
+
+  x[ 10],
+  x[ 11],
+  x[ 12],
+  x[ 13],
+  x[ 14],
+  x[ 15],
+  x[ 16],
+  x[ 17],
+  x[ 18],
+  x[ 19],
+
+  x[ 20],
+  x[ 21],
+  x[ 22],
+  x[ 23],
+  x[ 24],
+  x[ 25],
+  x[ 26],
+  x[ 27],
+  x[ 28],
+  x[ 29],
+
+  x[ 30],
+  x[ 31],
+  x[ 32],
+  x[ 33],
+  x[ 34],
+  x[ 35],
+  x[ 36],
+  x[ 37],
+  x[ 38],
+  x[ 39],
+
+  x[ 40],
+  x[ 41],
+  x[ 42],
+  x[ 43],
+  x[ 44],
+  x[ 45],
+  x[ 46],
+  x[ 47],
+  x[ 48],
+  x[ 49],
+
+  x[ 50],
+  x[ 51],
+  x[ 52],
+  x[ 53],
+  x[ 54],
+  x[ 55],
+  x[ 56],
+  x[ 57],
+  x[ 58],
+  x[ 59],
+
+  x[ 60],
+  x[ 61],
+  x[ 62],
+  x[ 63],
+  x[ 64],
+  x[ 65],
+  x[ 66],
+  x[ 67],
+  x[ 68],
+  x[ 69],
+
+  x[ 70],
+  x[ 71],
+  x[ 72],
+  x[ 73],
+  x[ 74],
+  x[ 75],
+  x[ 76],
+  x[ 77],
+  x[ 78],
+  x[ 79],
+
+  x[ 80],
+  x[ 81],
+  x[ 82],
+  x[ 83],
+  x[ 84],
+  x[ 85],
+  x[ 86],
+  x[ 87],
+  x[ 88],
+  x[ 89],
+
+  x[ 90],
+  x[ 91],
+  x[ 92],
+  x[ 93],
+  x[ 94],
+  x[ 95],
+  x[ 96],
+  x[ 97],
+  x[ 98],
+  x[ 99],
+
+
+  x[100],
+  x[101],
+  x[102],
+  x[103],
+  x[104],
+  x[105],
+  x[106],
+  x[107],
+  x[108],
+  x[109],
+
+  x[110],
+  x[111],
+  x[112],
+  x[113],
+  x[114],
+  x[115],
+  x[116],
+  x[117],
+  x[118],
+  x[119],
+
+  x[120],
+  x[121],
+  x[122],
+  x[123],
+  x[124],
+  x[125],
+  x[126],
+  x[127],
+  x[128],
+  x[129],
+
+  x[130],
+  x[131],
+  x[132],
+  x[133],
+  x[134],
+  x[135],
+  x[136],
+  x[137],
+  x[138],
+  x[139],
+
+  x[140],
+  x[141],
+  x[142],
+  x[143],
+  x[144],
+  x[145],
+  x[146],
+  x[147],
+  x[148],
+  x[149],
+
+  x[150],
+  x[151],
+  x[152],
+  x[153],
+  x[154],
+  x[155],
+  x[156],
+  x[157],
+  x[158],
+  x[159],
+
+  x[160],
+  x[161],
+  x[162],
+  x[163],
+  x[164],
+  x[165],
+  x[166],
+  x[167],
+  x[168],
+  x[169],
+
+  x[170],
+  x[171],
+  x[172],
+  x[173],
+  x[174],
+  x[175],
+  x[176],
+  x[177],
+  x[178],
+  x[179],
+
+  x[180],
+  x[181],
+  x[182],
+  x[183],
+  x[184],
+  x[185],
+  x[186],
+  x[187],
+  x[188],
+  x[189],
+
+  x[190],
+  x[191],
+  x[192],
+  x[193],
+  x[194],
+  x[195],
+  x[196],
+  x[197],
+  x[198],
+  x[199],
+
+  x[200],
+  x[201],
+  x[202],
+  x[203],
+  x[204],
+  x[205],
+  x[206],
+  x[207],
+  x[208],
+  x[209],
+
+  x[210],
+  x[211],
+  x[212],
+  x[213],
+  x[214],
+  x[215],
+  x[216],
+  x[217],
+  x[218],
+  x[219],
+
+  x[220],
+  x[221],
+  x[222],
+  x[223],
+  x[224],
+  x[225],
+  x[226],
+  x[227],
+  x[228],
+  x[229],
+
+  x[230],
+  x[231],
+  x[232],
+  x[233],
+  x[234],
+  x[235],
+  x[236],
+  x[237],
+  x[238],
+  x[239],
+
+  x[240],
+  x[241],
+  x[242],
+  x[243],
+  x[244],
+  x[245],
+  x[246]
+); // if it yells about fanin, have fun!
+// I know n-input xor gates are okay, but n is pretty high here so you
+// can just tone them down a bit if there's a problem, but you shouldn't need
+// to go down to 2 inputs each.
 endmodule
